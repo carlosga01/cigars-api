0a477e444ec4ac343a4061d5caec903015f061cd
Carlos Garcia <carlos305garcia@gmail.com>
2023-09-18 21:10:46 -0400

    Connect to postgresql

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask_restful import Resource, Api
+import psycopg2
 
 app = Flask(__name__)
 api = Api(app)
+
+conn = psycopg2.connect(database="cigarsdb",
+                            user="cigarsdb_user",
+                            password="40kMy0v70HP5Lc1PyipY8MBSJcvNi2t7",
+                            host="dpg-ck4dmqt8ggls73eb2grg-a.oregon-postgres.render.com", port="5432")
 
 class HelloWorld(Resource):
     def get(self):
